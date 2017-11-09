d25a5f5919b8e90414b2b0aa8cc11faf51b58b83
Ulas Tuerkmen <ulas.tuerkmen@gmail.com>
2017-11-09 23:07:37 +0100

    Replaced repo with registry, WIP test for mac os

--- twyla/kubedeploy/test/test_docker.py
+++ twyla/kubedeploy/test/test_docker.py
@@ -8,7 +8,7 @@ from twyla.kubedeploy import docker_helpers
 DOCKER_CREDENTIALS = "tim_toddler:crappy password"
 DOCKER_CONF = json.dumps({
     "auths": {
-        "myown.private.repo": {
+        "myown.private.registry": {
 	    "auth": str(base64.b64encode(DOCKER_CREDENTIALS.encode('utf-8')).decode('utf-8'))
         }
     }
@@ -17,14 +17,16 @@ DOCKER_CONF = json.dumps({
 class DockerTests(unittest.TestCase):
 
     def test_make_tag(self):
-        tag = docker_helpers.make_tag('myown.private.repo', 'the-service', '678fg')
-        assert tag == 'myown.private.repo/the-service:678fg'
+        tag = docker_helpers.make_tag('myown.private.registry',
+                                      'the-service',
+                                      '678fg')
+        assert tag == 'myown.private.registry/the-service:678fg'
 
 
     def test_tag_components(self):
         domain, name, version = docker_helpers.tag_components(
-            'myown.private.repo/the-service:678fg')
-        assert domain == 'myown.private.repo'
+            'myown.private.registry/the-service:678fg')
+        assert domain == 'myown.private.registry'
         assert name == 'the-service'
         assert version == '678fg'
 
@@ -35,10 +37,10 @@ class DockerTests(unittest.TestCase):
     def test_docker_image_exists(self, mock_registry):
         mock_client = mock_registry.DockerRegistryClient.return_value
         mock_client.repository.return_value.tags.return_value = ['678fg', '123gf']
-        exists = docker_helpers.docker_image_exists('myown.private.repo/the-service:678fg')
+        exists = docker_helpers.docker_image_exists('myown.private.registry/the-service:678fg')
         assert exists
         mock_registry.DockerRegistryClient.assert_called_once_with(
-            "https://myown.private.repo",
+            "https://myown.private.registry",
             username="tim_toddler",
             password="crappy password")
         mock_client.repository.assert_called_once_with("the-service")
@@ -50,5 +52,10 @@ class DockerTests(unittest.TestCase):
     def test_docker_image_exists(self, mock_registry):
         mock_client = mock_registry.DockerRegistryClient.return_value
         mock_client.repository.return_value.tags.return_value = ['123gf']
-        exists = docker_helpers.docker_image_exists('myown.private.repo/the-service:678fg')
+        exists = docker_helpers.docker_image_exists('myown.private.registry/the-service:678fg')
         assert not exists
+
+
+    @mock.patch('twyla.kubedeploy.docker_helpers.Popen')
+    def test_get_macos_credentials(self, mock_popen):
+        docker_helpers.get_macos_credentials('myown.private.registry')
